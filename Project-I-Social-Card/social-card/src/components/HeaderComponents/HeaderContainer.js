f8ae31aee30b4fc40cda7fda223216349a22d393
Marcus Kwan <kwan.marcus@gmail.com>
2019-05-27 08:37:35 +1000

    finish project1

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,21 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
+
+function HeaderContainer() {
+  return (
+    <div className="header-container">
+      <header>
+        <ImageThumbnail />
+        <div className="right-header">
+        <HeaderTitle />
+        <HeaderContent />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default HeaderContainer;
